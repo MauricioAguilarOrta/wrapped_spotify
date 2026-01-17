c99d6c98efb94f204f4eb30c79bcab1f8ea85b22
Adrián Mauricio Aguilar Orta <126046260+MauricioAguilarOrta@users.noreply.github.com>
2026-01-16 20:44:28 -0600

    Update wrapped_spotify.R
    Actualización a enero 2026

--- wrapped_spotify.R
+++ wrapped_spotify.R
@@ -282,13 +282,4 @@ server <- function(input, output, session) {
   })
 }
 
-#Aquí se ejecuta la aplicación con la interfaz y el servidor
-shinyApp(ui, server)
-
-# Si deseas publicarlo necesitas una cuenta en shiny.app (hay otras opciones pero ésta es la más sencilla). Encuentra las credenciales en tu perfil y pégalas aquí
-rsconnect::setAccountInfo(name='',
-                          token='',
-                          secret='')
-
-# Aquí se ejecuta con el path que definiste al principio, tu working directory.
-rsconnect::deployApp(path)
+# Es muy importante tener cuenta de Shiny y que ejecutes el siguiente código (tiene que estar separado el código que crea al tablero y el que crea al shiny)
